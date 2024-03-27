25a31b9247cb9c5c915dc9d07b2e3032bb8a75ea
admin <jyt2097@gmail.com>
2024-03-27 14:26:54 +0900

    4주차 문제 수정 완료

--- JS/search.js
+++ JS/search.js
@@ -1,5 +1,17 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
+document.getElementById("search_button_msg").addEventListener('click', search_message);
+
+let m = "검색을 수행합니다!"
 
 function search_message(){
-    alert("검색을 수행합니다!");
+    alert(m);
 }
+
+/*
+
+function search_message(){
+    alert("검색을 수행합니다!2222");
+} 
+// 같은 이름의 함수를 중첩하면 에러가 아닌 더 나중에 선언된 함수가 실행된다
+
+*/
+
