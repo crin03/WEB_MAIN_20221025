ce42b92c9403092036c9686544aab9de328170eb
admin <jyt2097@gmail.com>
2024-04-03 14:08:29 +0900

    5주차 검색, 팝업, 호버 추가 완료

--- JS/search.js
+++ JS/search.js
@@ -14,4 +14,14 @@ function search_message(){
 // 같은 이름의 함수를 중첩하면 에러가 아닌 더 나중에 선언된 함수가 실행된다
 
 */
+function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
 
+
+
+
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+    // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기
+    return false;
+}
