aee2e02d64df9d76fdb2bae443426c330a719e26
admin <jyt2097@gmail.com>
2024-04-09 15:04:08 +0900

    5주차 수업 공백, 비속어 문제 수정 완료

--- JS/search.js
+++ JS/search.js
@@ -14,11 +14,23 @@ function search_message(){
 // 같은 이름의 함수를 중첩하면 에러가 아닌 더 나중에 선언된 함수가 실행된다
 
 */
+
 function googleSearch() {
     const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
 
+    let word = ["바보", "멍청이", "똥개", "미친", "지랄"]; // 비속어 배열
 
+    if (searchTerm === "") {
+        alert("문자를 입력해주세요.");
+        return false;
+    } // 공백 검사
 
+    for (let i = 0; i < word.length; i++) {
+        if (searchTerm.includes(word[i])) {
+            alert("비속어가 포함되어있습니다.");
+            return false;
+        }
+    } // 비속어 검사
 
     const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
     // 새 창에서 구글 검색을 수행
