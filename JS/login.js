145d140a35488cce8a185dbcde087dfce7060044
admin <jyt2097@gmail.com>
2024-05-08 13:31:32 +0900

    10주차 쿠키 추가 완료

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,36 @@
+const idsave_check = document.getElementById('idSaveCheck');
+
+function setCookie (name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
+}
+
+function getCookie(name) {
+    var cookie = document.cookie;
+    console.log("쿠키를 요청합니다.");
+    if (cookie != "") {
+        var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) {
+            var cookie_name = cookie_array[index].split("=");
+            if (cookie_name[0] == "id") {
+                return cookie_name[1];
+            }
+        }
+    }
+    return ;
+}
+
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    const emailInput = document.getElementById('typeEmailX');
+    const idsave_check = document.getElementById('idSaveCheck');
+    let get_id = getCookie("id");
+    if(get_id) {
+        emailInput.value = get_id;
+        idsave_check.checked = true;
+    }
+}
+    
 const check_input = () => {
     const loginForm = document.getElementById('login_form');
     const loginBtn = document.getElementById('login_btn');
@@ -86,9 +119,19 @@ const check_input = () => {
     }
 
     console.log('이메일:', emailValue);
-    console.log('비밀번호:', passwordValue);
+    console.log('비밀번호:', passwordValue); 
+    
+    if(idsave_check.checked == true) { // 아이디 체크 o
+        alert("쿠키를 저장합니다.", emailValue);
+        setCookie("id", emailValue, 1); // 1일 저장
+        alert("쿠키 값 :" + emailValue);
+    }
+    else
+    { // 아이디 체크 x
+        setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
+    }
+    
     loginForm.submit();
-
-    };
+};
     
     document.getElementById("login_btn").addEventListener('click', check_input);
